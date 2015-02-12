b89f74cb8c2a0354e8be6dd5d0fdfbbbd3095791
Yusuke Higaki <yusuke.higaki@dzb.jp>
2015-02-12 15:28:52 +0900

    database settings and add entity

--- app/autoload.php
+++ app/autoload.php
@@ -7,7 +7,10 @@ use Composer\Autoload\ClassLoader;
  * @var ClassLoader $loader
  */
 $loader = require __DIR__.'/../vendor/autoload.php';
-
+$loader->add('Gedmo', __DIR__ . '/../vendor/gedmo-doctrine-extensions/lib');
+$loader->add('Stof', __DIR__ . '/../vendor/bundles');
+$loader->add('Doctrine\\Bundle',  __DIR__ . '/../vendor/bundles');
+$loader->add('Doctrine\\Common\\DataFixtures',  __DIR__ . '/../vendor/doctrine-fixtures/lib');
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 return $loader;
---
+++ src/Dzb/RentacarBundle/DataFixtures/ORM/LoadLocationData.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Dzb\RentacarBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Dzb\RentacarBundle\Entity\Location;
+
+class LoadLocationData extends AbstractFixture implements OrderedFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $location1 = new Location();
+        $location1->setId(1);
+        $location1->setName('神楽坂');
+
+        $location2 = new Location();
+        $location2->setId(2);
+        $location2->setName('青山');
+
+        $manager->persist($location1);
+        $manager->persist($location2);
+        $manager->flush();
+
+        $this->addReference('location-1', $location1);
+        $this->addReference('location-2', $location2);
+    }
+
+    public function getOrder()
+    {
+        return 1;
+    }
+}
